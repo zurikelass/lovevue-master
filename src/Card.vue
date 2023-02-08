25d387974e254a77c796e923f129991a6224075e
zurikelass <zuragogberashvili93@gmail.com>
2023-02-08 04:05:26 +0400

    New project zura

---
+++ src/Card.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: ['card'],
+  methods: {
+    addToWatchlist(card) {
+      this.$emit('add-to-watchlist', card)
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="card">
+      <img :src="card.coverImage"/>
+      <h3>{{ card.name }}</h3>
+      <p>{{ card.description }}</p>
+      <a :href="card.url">{{ card.url }}</a>
+      <br/>
+      <button class="watchbtn" @click="addToWatchlist(card)">Add to Watchlist</button>
+    </div>
+  </template>
+  
+ 
---
+++ src/WatchlistItem.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: ['item'],
+  methods: {
+    removeFromWatchlist(item) {
+      this.$emit('remove-from-watchlist', item)
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="watchlist-item">
+      <img :src="item.coverImage"/>
+      <h3>{{ item.name }}</h3>
+      <p>{{ item.description }}</p>
+      <a :href="item.url">{{ item.url }}</a>
+  
+      <button @click="removeFromWatchlist(item)">Remove from Watchlist </button>
+    </div>
+  </template>
+  
+ 
